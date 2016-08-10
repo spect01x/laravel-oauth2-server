1f408a2a2708d7bfcc8f0998160a2df269b088de
Apti Umayev <apoq@protonmail.ch>
2016-08-10 12:11:43 +0200

    add pgsql models

---
+++ src/Models/Pgsql/AuthCodeModel.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace RTLer\Oauth2\Models\Pgsql;
+
+use Illuminate\Database\Eloquent\Model;
+
+class AuthCodeModel extends Model
+{
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'oauth_auth_codes';
+
+    /**
+     * The attributes that should be mutated to dates.
+     *
+     * @var array
+     */
+    protected $dates = [
+        'expire_time',
+    ];
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'token',
+        'client_id',
+        'expire_time',
+        'user_id',
+    ];
+}
